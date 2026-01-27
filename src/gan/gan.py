c31d8410bdc4c2577acf9afb82e4b01bb34b2b77
Pothan Nallabothula <pothan500@gmail.com>
2026-01-27 18:33:23 +0000

    made sure that only the final GAN class is defined and implemented in this file

--- src/gan/gan.py
+++ src/gan/gan.py
@@ -1,154 +1,66 @@
-import numpy as np
+from layers import GeneratorLayer, GeneratorOutputLayer, DiscriminatorLayer, DiscriminatorOutputLayer
 
-class Layer:
-    def __init__(self, input_dim, output_dim):
-        # Initialise everything except the Weight matrix because there's a different scale factor in each class
-        self.B = np.zeros((1, output_dim))
+class GAN:
+    def __init__(self, latent_vector_dim, output_dim, learning_rate=0.001):
+        self.input_dim = latent_vector_dim  # dimensionality of the latent vector Z_rand
+        self.output_dim = output_dim    # dimensionality of an MNIST image is 28*28 = 784
+        self.learning_rate = learning_rate
+
+        self.generator_layers = [
+            GeneratorLayer(self.input_dim, 256),
+            GeneratorLayer(256, 512),
+            GeneratorOutputLayer(512, self.output_dim)
+        ]
+
+        self.discriminator_layers = [
+            DiscriminatorLayer(self.output_dim, 512),
+            DiscriminatorLayer(512, 256),
+            DiscriminatorOutputLayer(256, 1)
+        ]
+
+    
+    def gen_forward(self, Z):
+        A = Z
+        for layer in self.generator_layers:
+            A = layer.forward(A)
         
-        self.X_input = None
-
-        # Initialise gradients with Zeros (so we can add to them later during backpropagation)
-        self.dL_dW = np.zeros((input_dim, output_dim))
-        self.dL_dB = np.zeros((1, output_dim))
+        gen_output = A
+        return gen_output
     
 
-    def forward(self):
-        return "Forward Pass"
+    def disc_forward(self, X):
+        A = X
+        for layer in self.discriminator_layers:
+            A = layer.forward(A)
+
+        disc_output = A
+        return disc_output
+
+
+    def disc_backward(self, y_target):
+        gradient = self.discriminator_layers[-1].backward(y_target)
+
+        for layer in self.discriminator_layers[-2::-1]:
+            gradient = layer.backward(gradient)
+        
+        return gradient
+
+
+    def gen_backward(self, gradient_from_discriminator):
+        gradient = gradient_from_discriminator
+
+        for layer in self.generator_layers[::-1]:
+            gradient = layer.backward(gradient)
+        
+        return gradient
     
-
-    def backward(self):
-        return "Backward Pass"
-
-
-    def _relu(self, matrix):
-        return np.maximum(0, matrix)
-    
-
-    def _relu_derivative(self, x):
-        # Derivative of _ReLU: 1 if x > 0, else 0
-        return (x > 0).astype(float)
-
-
-    def _sigmoid(self, matrix):
-        return 1 / (1 + np.exp(-matrix))
-
-    
-    def _sigmoid_derivative(self, matrix):
-        return self._sigmoid(matrix) * (1 - self._sigmoid(matrix))
-
 
     def reset_gradients(self):
-        # We need to empty the bucket before a new epoch
-        self.dL_dW.fill(0)
-        self.dL_dB.fill(0)
+        for layer in (self.generator_layers + self.discriminator_layers):
+            layer.set_gradients_zero()
     
 
-class DiscriminatorOutputLayer(Layer):
-    def __init__(self, input_dim, output_dim):
-        super().__init__(input_dim, output_dim)
-        
-        scale_factor = np.sqrt(1.0 / input_dim)
-        self.W = np.random.randn(input_dim, output_dim) * scale_factor
-
-
-    def forward(self, previous_layer_A):
-        self.X_input = previous_layer_A # input to the output layer is the post-activation output from the previous layer
-        self.Z = np.dot(self.X_input, self.W) + self.B
-        self.A = self._sigmoid(self.Z)
-
-        return self.A # this A will be the prediction
-
-
-    def backward(self, y_target):
-        # If Real: (A - 1)
-        # If Fake: (A - 0)
-        error = self.A - y_target
-
-        self.dL_dW += np.dot(self.X_input.T, error)
-        self.dL_dB += np.sum(error, axis=0, keepdims=True)
-
-        gradient_to_pass_back = np.dot(error, self.W.T)
-
-        return gradient_to_pass_back
-
-
-class DiscriminatorLayer(Layer):
-    def __init__(self, input_dim, output_dim):
-        super().__init__(input_dim, output_dim)
-
-        scale_factor = np.sqrt(2.0 / input_dim)
-        self.W = np.random.randn(input_dim, output_dim) * scale_factor
-
-
-    def forward(self, previous_layer_A):
-        self.X_input = previous_layer_A
-        self.Z = np.dot(self.X_input, self.W) + self.B
-        self.A = self._relu(self.Z)
-        
-        return self.A
-
-
-    def backward(self, gradient_flowing_back):
-        error = gradient_flowing_back * self._relu_derivative(self.Z)
-        
-        self.dL_dW += np.dot(self.X_input.T, error)
-        self.dL_dB += np.sum(error, axis=0, keepdims=True)
-
-        gradient_to_pass_back = np.dot(error, self.W.T)
-
-        return gradient_to_pass_back
-
-
-class GeneratorOutputLayer(Layer):
-    def __init__(self, input_dim, output_dim):
-        super().__init__(input_dim, output_dim)
-        
-        scale_factor = np.sqrt(1.0 / input_dim)
-        self.W = np.random.randn(input_dim, output_dim) * scale_factor
-
-
-    def forward(self, previous_layer_A):
-        self.X_input = previous_layer_A
-        self.Z = np.dot(self.X_input, self.W) + self.B
-        self.A = self._sigmoid(self.Z)
-        
-        return self.A
-
-
-    def backward(self, gradient_flowing_back_from_discriminator):
-        error = gradient_flowing_back_from_discriminator * self._sigmoid_derivative(self.Z) 
-
-        self.dL_dW += np.dot(self.X_input.T, error)
-        self.dL_dB += np.sum(error, axis=0, keepdims=True)
-
-        gradient_to_pass_back = np.dot(error, self.W.T)
-
-        return gradient_to_pass_back
-
-
-class GeneratorLayer(Layer):
-    def __init__(self, input_dim, output_dim):
-        super().__init__(input_dim, output_dim)
-
-        scale_factor = np.sqrt(2.0 / input_dim)
-        self.W = np.random.randn(input_dim, output_dim) * scale_factor
-
-
-    def forward(self, previous_layer_A):
-        self.X_input = previous_layer_A
-        self.Z = np.dot(self.X_input, self.W) + self.B
-        self.A = self._relu(self.Z)
-        
-        return self.A
-
-
-    def backward(self, gradient_flowing_back):
-        error = gradient_flowing_back * self._relu_derivative(self.Z)
-        
-        self.dL_dW += np.dot(self.X_input.T, error)
-        self.dL_dB += np.sum(error, axis=0, keepdims=True)
-
-        gradient_to_pass_back = np.dot(error, self.W.T)
-
-        return gradient_to_pass_back
-
+    def GD_update_step(self):
+        for layer in (self.generator_layers + self.discriminator_layers):
+            layer.W -= self.learning_rate * layer.dL_dW
+            layer.B -= self.learning_rate * layer.dL_dB
